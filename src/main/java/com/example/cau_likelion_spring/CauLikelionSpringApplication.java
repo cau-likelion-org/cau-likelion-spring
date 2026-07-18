0a2f6e5bb4c8a64287292f2e7baacdc52aaccd13
only4wxx <orchidbyw1@gmail.com>
2026-07-18 18:42:26 +0900

    init: 스프링 프로젝트 생성 (#1)

---
+++ src/main/java/com/example/cau_likelion_spring/CauLikelionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.cau_likelion_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CauLikelionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CauLikelionSpringApplication.class, args);
+	}
+
+}

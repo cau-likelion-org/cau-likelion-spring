76904eac06ff49a438cca16f4b69864c8c28e14e
ziwonii <nayanako03@gmail.com>
2026-07-22 02:45:53 +0900

    fix: platform fixed

--- src/main/java/com/example/cau_likelion_spring/project/domain/Platform.java
+++ src/main/java/com/example/cau_likelion_spring/project/domain/Platform.java
@@ -2,10 +2,9 @@ package com.example.cau_likelion_spring.project.domain;
 
 /**
  * 프로젝트 링크에 사용되는 플랫폼
- * TODO: 종류 최종 결정되면 수정 필요
  */
 public enum Platform {
     GITHUB,
     WEB,
-
+    BEHANCE,
 }

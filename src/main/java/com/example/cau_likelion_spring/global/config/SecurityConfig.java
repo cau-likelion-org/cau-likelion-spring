d929ac09665f2fc1510222ad15dacdeb9024267e
ziwonii <nayanako03@gmail.com>
2026-07-25 10:44:46 +0900

    feat: project 조회 permitall로 변경

--- src/main/java/com/example/cau_likelion_spring/global/config/SecurityConfig.java
+++ src/main/java/com/example/cau_likelion_spring/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.cau_likelion_spring.global.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,6 +37,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PERMIT_ALL_PATHS).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/projects/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);

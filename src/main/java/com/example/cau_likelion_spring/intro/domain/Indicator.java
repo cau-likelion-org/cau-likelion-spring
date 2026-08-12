780fab23d41981fb7d5befcd06d09cfc95212408
only4wxx <orchidbyw1@gmail.com>
2026-08-12 20:19:22 +0900

    feat: Indicator 모델 생성자 및 업데이트 함수 추가 (#64)

--- src/main/java/com/example/cau_likelion_spring/intro/domain/Indicator.java
+++ src/main/java/com/example/cau_likelion_spring/intro/domain/Indicator.java
@@ -6,11 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
- * 랜딩 페이지 FAQ (질문 및 답변)
+ * 랜딩 페이지 정량 지표 (누적 기수 / 누적 수료자 / 누적 프로젝트)
+ * 항상 단 하나의 row만 존재해야 한다 - IndicatorService가 조회 시점에 없으면 기본값 0으로 생성해서 이를 보장한다.
  */
 @Entity
 @Getter
@@ -29,4 +31,17 @@ public class Indicator extends BaseTimeEntity {
 
     // 누적 프로젝트
     private String cumulative_projects;
+
+    @Builder
+    private Indicator(String cumulative_generations, String cumulative_graduates, String cumulative_projects) {
+        this.cumulative_generations = cumulative_generations;
+        this.cumulative_graduates = cumulative_graduates;
+        this.cumulative_projects = cumulative_projects;
+    }
+
+    public void update(String cumulativeGenerations, String cumulativeGraduates, String cumulativeProjects) {
+        this.cumulative_generations = cumulativeGenerations;
+        this.cumulative_graduates = cumulativeGraduates;
+        this.cumulative_projects = cumulativeProjects;
+    }
 }
